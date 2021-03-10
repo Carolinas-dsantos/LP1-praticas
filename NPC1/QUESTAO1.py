c2964f27b7708696d240d9164cf70b3d93be81fe
Carolinas-dsantos <carolin.silva@aluno.uece.br>
2021-03-10 19:46:41 -0300

    prova1

---
+++ NPC1/QUESTAO1.py
@@ -0,0 +1,22 @@
+#Converta as seguintes expressões matemáticas para que possam ser calculadas usando Python:
+#100² / 3 + 24
+#(9⁴ + 2) x (6 + 34⁴)
+#(9⁴ + 2)⁴ + ( 10 / 1)
+
+x = 100
+y = 3
+z = 24
+
+print(f"100² / 3 + 24 = {x**2 / y + z}")
+
+c = 9
+d = 2
+e = 6
+f = 34
+print(f"(9⁴ + 2) x (6 + 34⁴) = {(9**4 + 2) * (6 + 34**4)}")
+
+m = 9
+n = 2
+o = 10 
+p = 1
+print(f"(9⁴ + 2)⁴ + ( 10 / 1) = {(9**4 + 2) + (10 / 1)}")
