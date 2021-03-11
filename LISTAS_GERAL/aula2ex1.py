5ed7232f4af8c0187f621f1fd07a72d2ffd2fb99
Carolinas-dsantos <carolin.silva@aluno.uece.br>
2021-03-10 22:17:08 -0300

    LISTAS

---
+++ LISTAS_GERAL/aula2ex1.py
@@ -0,0 +1,24 @@
+nome = input("Seu nome: ")
+idade = int(input("Sua idade: "))
+estado_civil = input("Seu estado civil: ")
+genero = input("Seu genero é: ")
+
+def nome (n):
+    return n
+
+def idade (i):
+    return i 
+
+def estado_civil (ec):
+    return ec
+
+def genero (g):
+    return g 
+
+    print(nome (n))
+
+    print(idade(i))
+
+    print(estado_civil(ec))
+
+    print(genero(g))
---
+++ LISTAS_GERAL/dist_passag.py
@@ -0,0 +1,12 @@
+# Escreva um programa que pergunte a distância que um passageiro deseja
+# percorrer em km. Calcule o preço da passagem, cobrando R$ 0.50 por km
+# para viagens de até 200 km, e R$ 0.45 para viagens mais longas.
+
+distancia= int(input("Digite a distância da sua viagem, Km: "))
+
+if distancia <= 200:
+    passagem = distancia * 0.50
+else:
+    passagem = distancia * 0.45
+
+print(f"O valor da sua passagem é de R${passagem}")
